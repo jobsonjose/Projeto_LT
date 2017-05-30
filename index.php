9147c1d358c9fbd86b173ca29eaebe746c4bb568
juliotoscano <juliotoscanodebrito@gmail.com>
2017-05-30 00:39:41 -0300

    INserino jquery

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
-<?php 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="css/Style.css">
+    <script type="text/javascript" src="jquery-3.2.1.js"></script>
+</head>
+<body>
+    <form action="php/Login.php" method="POST" id="Login">
+    <p>Sistema de Login</p>
+        <label>
+            E-mail :
+            <input type="email" name="email.txp">
+            <br>
+        </label>
+        <label>
+            Senha :
+            <input type="password" name="senha.txp">
+            <br>
+        </label>
+        <input type="submit" name="Enviar" value="Conecte Agora">
+    </form>
 
-
-
-?>
+</body>
+</html>
