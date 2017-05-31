af6f33795355744007c97649275ab2d4be28160d
juliotoscano <juliotoscanodebrito@gmail.com>
2017-05-30 23:13:33 -0300

    BEM FIz a conexao e o INsert Estao funcionando  , pode testar

--- php/home.php
+++ php/home.php
@@ -1,12 +1,5 @@
 <?php
-    $dbname ='projeto_LT';
-    $host = 'localhost';
-    $user = 'root';
-    $pass = '123456';
 
-    $dsn = "mysql:dbname=$dbname;host=$host";
-
-    $pdo = new PDO($dsn,$user,$pass);
 
 
 
---
+++ php/insert.php
@@ -0,0 +1,30 @@
+<?php
+        $dbname ='PROJETO_LT';
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '123456';
+
+    $dsn = "mysql:dbname=$dbname;host=$host";
+
+    $pdo = new PDO($dsn,$user,$pass);
+    var_dump($_POST);
+    $nome = $_POST['nome_txp'];
+    $email = $_POST['email_txp'];
+    $senha = $_POST['senha_txp'];
+    $telefone = $_POST['telefone_txp'];
+    $usuario = $_POST['funcao_txp'];
+
+    if($usuario == usuario){
+        $ret = $pdo->exec("INSERT INTO USUARIO (USER_NOME, USER_EMAIL, USER_SENHA, USER_NUM_CELULAR) values ('$nome', '$email', '$senha', '$telefone') ");
+    }
+    if($usuario == supervisor) {
+        $rot = $pdo->exec("INSERT INTO SUPERVISOR (SUPER_NOME, SUPER_EMAIL, SUPER_SENHA, SUPER_TELE) values('$nome', '$email', '$senha', '$telefone')");
+        var_dump($rot);
+   }else{
+        echo "ERROU";
+    }
+
+
+
+
+?>
